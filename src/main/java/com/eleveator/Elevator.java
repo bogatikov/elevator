b05403a186013c7a38e12fcbef772afd5892378f
Tony <al.proudly@yandex.ru>
2019-06-16 14:59:12 +0300

    Similating elevator

--- src/main/java/com/eleveator/Elevator.java
+++ src/main/java/com/eleveator/Elevator.java
@@ -1,92 +1,182 @@
 package com.eleveator;
 
-import java.util.Observable;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.ArrayList;
+import java.util.List;
 
 enum Status {
     UP,
-    BOTTOM,
+    DOWN,
     WAIT
 }
-public class Elevator extends Observable {
+public class Elevator {
 
     private static final int FLOORS = 7;
-    private int currentFloor = 7;
-    private int speed = 10; // seconds per floor
-    private int delay = 2; //
+    private int startFloor = 7;
     private Status status = Status.WAIT;
-    private Queue<Integer> queue;
-
+    private List<Dir> queue;
 
 
     public Elevator() {
-        queue = new ConcurrentLinkedQueue<> ();
+        queue = new ArrayList<>();
     }
 
     // Call the elevator
     public void call(int floor) throws Exception {
         if (floor > FLOORS || floor < 0) throw new Exception("Wrong way");
-        if (currentFloor == floor) return;
-        queue.add(floor);
+        queue.add(new Dir(floor));
     }
 
-    void move() {
+
+    private boolean queueContain(int floor) {
+        for (Dir d:
+             queue) {
+            if (d.moveTo == floor) {
+                return true;
+            }
+        }
+        return false;
+    }
+    String getCurrentStatus () {
+        long now = System.currentTimeMillis();
+        long time = queue.get(0).start;
+        int currentFloor = startFloor;
+        int currentGoal = queue.get(0).moveTo;
+        for (Dir d:
+             queue) {
+            d.applyed = false;
+        }
+
+        while (hasNotApplyed()){
+            status = (currentFloor > currentGoal) ? Status.DOWN : Status.UP;
+            if (status == Status.UP) {
+                for (int k = 0; k < queue.size(); k++) {
+                    Dir d = queue.get(k);
+                    if (d.moveTo > currentGoal && d.start - time <= (Math.abs(currentFloor-currentGoal) * 10000)) {
+                        d.applyed = true;
+                        currentGoal = d.moveTo;
+                    }
+                }
+
+
+                for (int k = currentFloor; k <= currentGoal; k++) {
+
+                    if (now - time >= 10000) {
+                        if (k == currentGoal) {
+                            if (now - time <= 12000)
+                                return "Open door on " + currentFloor;
+                            else if (now - time <= 14000)
+                                return "Close door on " + currentFloor;
+                            time += 4000;
+                        }
+                        time += 10000;
+                        currentFloor++;
+                    }  else {
+                        return "Moving on " + currentFloor + " to " + currentGoal;
+                    }
+                }
+            }
+            if (status == Status.DOWN) {
+                for (int k = 0; k < queue.size(); k++) {
+                    Dir d = queue.get(k);
+                    if (d.moveTo < currentGoal && d.start - time <= (Math.abs(currentFloor-currentGoal) * 10000)) {
+                        d.applyed = true;
+                        currentGoal = d.moveTo;
+                    }
+                }
+
+
+                for (int k = currentFloor; k >= currentGoal; k--) {
+
+                    if (now - time >= 10000) {
+                        if (queueContain(currentFloor)) {
+                            if (now - time <= 12000)
+                                return "Open door on " + currentFloor;
+                            else if (now - time <= 14000)
+                                return "Close door on " + currentFloor;
+                            time += 4000;
+                        }
+                        time += 10000;
+                        currentFloor--;
+                    }  else {
+                        return "Moving on " + currentFloor + " to " + currentGoal;
+                    }
+                }
+            }
+        }
+
+        return "Nothing to do on " + currentFloor;
+    }
+
+    private boolean hasNotApplyed() {
+        boolean res = false;
+        for (Dir dir:
+             queue) {
+            if (!dir.applyed)
+                return true;
+        }
+        return res;
+    }
+
+        /*    String getCurrentStatus() {
+            long now = System.currentTimeMillis();
+            if (queue.size() == 0) {
+                return "Waiting command";
+            }
+            int currentFloor = startFloor;
+            long time;
+            time = queue.get(0).start;
+
+            for (int k = 0; k < queue.size(); k++) {
+                Dir d = queue.get(k);
+                if (status == Status.UP) {
+                    for (int j = k; j < queue.size(); j++) {
+                        if (queue.get(j).moveTo > d.moveTo) {
+                            d = queue.get(j);
+                        }
+                    }
+                }
+                if (status == Status.DOWN) {
+                    for (int j = k; j < queue.size(); j++) {
+                        if (queue.get(j).moveTo < d.moveTo) {
+                            d = queue.get(j);
+                        }
+                    }
+                }
+                status = d.moveTo > currentFloor ? Status.UP : Status.DOWN;
+                for (int i = 0; i < Math.abs(d.moveTo - currentFloor); i++) {
+
+                    long dt = now - time;
+                    if (dt >= 10000) {
+                        if (queueContain(currentFloor) && status == Status.DOWN) {
+                            if (dt <= 12000) {
+                                return "Open door on " + currentFloor;
+                            } else if (dt <= 14000) {
+                                return "Close door on " + currentFloor;
+                            }
+                            time += 4000;
+                        }
+                        time += 10000;
+                        currentFloor = (status == Status.DOWN) ? --currentFloor : ++currentFloor;
+                    } else {
+                        return "Moving " + status.name() + " on " + currentFloor;
+                    }
+                }
+            }
+
+            return "Nothing to do";
+        }*/
+
+
+
+    class Dir {
         long start;
-        while (queue.size() > 0) {
+        int moveTo;
+        boolean applyed;
 
-            String mes = "";
-            for (Integer i:
-                 queue) {
-                mes += " " + i;
-            }
-            setChanged();
-            notifyObservers("Queue: " + mes);
-
-
-            int currentGoal = queue.poll();
-
-            while (currentFloor != currentGoal) {
-                if (currentGoal > currentFloor) status = Status.UP;
-                else status = Status.BOTTOM;
-
-                start = System.currentTimeMillis();
-
-                setChanged();
-                notifyObservers("Current floor: " + currentFloor +  " Current goal: "+ currentGoal + " Moving " + status.name());
-
-                //
-                while (System.currentTimeMillis() - start < speed * 1000) {
-                    // Elevator moves
-                }
-                currentFloor = (status == Status.BOTTOM) ? --currentFloor : ++currentFloor;
-
-
-                if (queue.contains(currentFloor)) {
-                    queue.remove(currentFloor);
-                    stopOnTheFloor(currentFloor);
-                }
-            }
-
-            stopOnTheFloor(currentFloor);
+        public Dir(int moveTo) {
+            this.start = System.currentTimeMillis();
+            this.moveTo = moveTo;
         }
-        status = Status.WAIT;
-        notifyObservers(status.name());
     }
-
-    void stopOnTheFloor(int floor){
-        setChanged();
-        notifyObservers("Stop at " + floor);
-        long start = System.currentTimeMillis();
-        // Stop and start delay
-        while ((System.currentTimeMillis() - start < 2*delay * 1000)) {
-            if (System.currentTimeMillis() - start < 2000) {
-                // stop
-            }
-        }
-        setChanged();
-        notifyObservers("Start from " + floor);
-    }
-
 }
 
--- src/main/java/com/eleveator/Main.java
+++ src/main/java/com/eleveator/Main.java
@@ -3,17 +3,21 @@ package com.eleveator;
 public class Main {
     public static void main(String[] args) throws Exception {
         Elevator elevator = new Elevator();
-        elevator.addObserver((o, arg) -> System.out.println((String) arg));
-        elevator.call(7);
-        elevator.call(6);
-        elevator.call(4);
-        elevator.move();
-
-
-        elevator.call(7);
-        elevator.call(4);
         elevator.call(3);
-        elevator.call(6);
-        elevator.move();
+        elevator.call(5);
+        elevator.call(2);
+//        Thread.sleep(2000);
+//        elevator.call(5);
+//        Thread.sleep(2000);
+//        elevator.call(7);
+//        Thread.sleep(2000);
+//        elevator.call(6);
+//        Thread.sleep(2000);
+//        elevator.call(2);
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(elevator.getCurrentStatus());
+            Thread.sleep(2000);
+        }
     }
 }
